c289521178fa3f03981c491dba90a5f1a23b99a2
Santiago Torres Alvarez <55812138+Chewbacaguy@users.noreply.github.com>
2022-05-03 23:42:41 -0500

    Update AppCtaSection.jsx

--- components/molecules/AppCtaSection.jsx
+++ components/molecules/AppCtaSection.jsx
@@ -8,7 +8,7 @@ const AppCtaSection = () => (
       </h2>
       <p className="text-xs md:text-sm text-gray max-w-[260px] md:max-w-xs mx-auto mt-2">
         I am more than happy to meet you! You can contact me through any of my social media links and 
-        I will most definetly answer. If you have any proyect ideas that you want to finally see come to life...maybe I'm the guy to help you out. 
+        I will most definetly answer. If you have any proyect ideas that you want to finally see come to life...maybe I am the guy to help you out. 
       </p>
       <AppButton title="Say Hi.." href="/contact" color="primary" />
     </div>
