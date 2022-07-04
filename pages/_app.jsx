1ea99467cf1f96769773e3f17893ec2c399f8b06
Santiago Torres Alvarez <55812138+Chewbacaguy@users.noreply.github.com>
2022-07-04 14:05:50 -0500

    took firebase out

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,10 @@
 import { useEffect } from 'react';
-import { analytics } from '../lib/firebase';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => {
   useEffect(() => {
     if (process.env.NODE_ENV === 'production') {
-      analytics();
+      
     }
   }, []);
 
