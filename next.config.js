d4246504e9df7e71fab6dd8cca4b4a298e6dc062
Santiago Torres Alvarez <55812138+Chewbacaguy@users.noreply.github.com>
2022-07-04 14:38:04 -0500

    next config file

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 const withPwa = require('next-pwa');
-
 module.exports = withPwa({
   reactStrictMode: true,
   pwa: {
     dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-  },
-  images: {
-    domains: ['firebasestorage.googleapis.com'],
+    // disable: process.env.NODE_ENV === 'development',
   },
 });
