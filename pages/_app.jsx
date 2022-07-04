887b25c3ec94aa195dd5072e223599eacef80251
Santiago Torres Alvarez <55812138+Chewbacaguy@users.noreply.github.com>
2022-07-04 14:24:44 -0500

    try2

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,11 @@
 import { useEffect } from 'react';
+import { analytics } from '../lib/firebase';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => {
   useEffect(() => {
     if (process.env.NODE_ENV === 'production') {
-      
+      analytics();
     }
   }, []);
 
